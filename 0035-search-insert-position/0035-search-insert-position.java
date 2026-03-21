c59eb121dee2207180367906facbadffef56708f
Himanshu <68156080+Champhimu@users.noreply.github.com>
2026-03-21 22:07:14 +0530

    Time: 0 ms (100%), Space: 44.7 MB (78.78%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,10 +2,8 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int l=0;
         int r=nums.length-1;
-        int ans= nums.length-1;
 
         while(l<=r){
-            System.out.println(l + " "+ r);
             int mid = l+(r-l)/2;
 
             if(nums[mid] == target){
@@ -13,15 +11,11 @@ class Solution {
             }
 
             if(nums[mid] < target){
-                if(nums.length - 1 == l ){
-                    ans++;
-                }
                 l = mid + 1;
             }else{
-                ans = mid;
                 r = mid - 1;
             }
         }
-        return ans;
+        return l;
     }
 }
