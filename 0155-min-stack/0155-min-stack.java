cb9438efed1a70f273c86448df569b53933142c9
Himanshu <68156080+Champhimu@users.noreply.github.com>
2026-04-01 22:14:40 +0530

    Time: 5 ms (85.05%), Space: 46.8 MB (88.61%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        // Push to minStack if it's empty or val <= current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int removed = stack.pop();
+
+        // Sync minStack
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
